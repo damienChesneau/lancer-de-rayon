856e9ac1bf7816427fc683b4f3acd18a45e637ab
Steeve Sivanantham <steeve.sivanantham@gmail.com>
2016-04-17 19:53:46 +0200

    Reading in file implementing

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include <g3x.h>
 
 
-int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint i);
-int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i);
-int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i);
+int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint ri);
+int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri);
+int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri);
+void initObjects(char* src);
+char* getOpt(char* opt,int argc,char* argv[]);
+void doLevel1(int argc,char* argv[]);
+void doLevel2(int argc,char* argv[]);
+void doLevel3(int argc,char* argv[]);
+void doLevel4(int argc,char* argv[]);
 
 typedef struct{
 	G3Xpoint a;
 	G3Xpoint b;
 	G3Xpoint c;
+	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 }Triangle;
 
 typedef struct{
@@ -38,8 +45,8 @@ typedef struct{
 }Rayon;
 
 typedef struct{
-	void *this;
-	G3Xhmat transform;
+	int this;
+	G3Xhmat transfo;
 	G3Xhmat inverse;
 }Object;
 
@@ -48,13 +55,13 @@ Triangle triangle = {
 	{0,0,0}, 
 	{1,0,0}, 
 	{0,1,0},	
-	&(rayInterTriangle)
+	rayInterTriangle
 };
 
 Sphere sphere = {
 	{0,0,0},
 	1,
-	&(rayInterCercle)
+	rayInterCercle
 };
 
 Rectangle rectangle = {
@@ -62,41 +69,73 @@ Rectangle rectangle = {
 	{1,0,0},
 	{0,1,0},
 	{0,0,1},
-	&(rayInterRectangle)
+	rayInterRectangle
 };
 
 
 Cylindre cylindre = {
 	{0,0,0},
 	1,
-	{0,0,1}
+	{0,0,1},
+	NULL
 };
 
-G3Xcamera camera;
+
+
+Object camera;
 Object objects[100];
 int nbObjects = 0;
 float image[32][32];
 /*****************************************************************************************************/
 
-int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint i){
-	double ps = G3Xprodscal((G3Xvector)pos,dir);
+void initObjects(char* src){
+	FILE* fichier = fopen(src,"r");
+	if (fichier != NULL){
+		int type = 0;
+		while(fscanf(fichier,"%d\t",&type)){
+			int i = 0;
+			if(type != 4){
+				objects[nbObjects].this = type;
+				for(i = 0; i<16; i++){
+					if(fscanf(fichier,"%lf ",&(objects[nbObjects].transfo[i])));
+				}
+				for(i = 0; i<16; i++){
+					if(fscanf(fichier,"%lf ",&(objects[nbObjects].inverse[i])));
+				}
+				nbObjects++;
+			}else{
+				for(i = 0; i<16; i++){
+					if(fscanf(fichier,"%lf ",&(camera.transfo[i])));
+				}
+				for(i = 0; i<16; i++){
+					if(fscanf(fichier,"%lf ",&(camera.inverse[i])));
+				}
+			}
+			fscanf(fichier, "\n");
+		}
+	}
+	fclose(fichier);
+}
+
+int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint ri){
+	double ps = G3Xprodscal(pos,dir);
 	if(ps>= 0 ){
 		return 0;
 	}
-	double d2 = G3Xsqrvnorm((G3Xvector)pos) - ps*ps;
+	double d2 = G3Xsqrvnorm(pos) - ps*ps;
 	if(d2>= 1){
 		return 0;
 	}
 	double t = -ps - sqrt(1-d2);
 	/*j'ai mon contact*/
-	i[0] = pos[0] + t * dir[0];
-	i[1] = pos[1] + t * dir[1];
-	i[2] = pos[2] + t * dir[2];
+	ri[0] = pos[0] + t * dir[0];
+	ri[1] = pos[1] + t * dir[1];
+	ri[2] = pos[2] + t * dir[2];
 	return 1;
 }
 
 
-int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
+int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri){
 	G3Xvector normal = {1,0,0};
 	double ps = G3Xprodscal(normal,dir);
 	if(ps>=0){
@@ -119,7 +158,7 @@ int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
 	return 1;
 }
 
-int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
+int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri){
 	G3Xvector normal[6] = {
 		{1,0,0},
 		{-1,0,0},
@@ -127,16 +166,16 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
 		{0,-1,0},
 		{0,0,1},
 		{0,0,-1}
-	}
+	};
 	int flags[6] = {0,0,0,0,0,0};
-	int ps[3] = {0,0,0}
-	double ps[0] = G3Xprodscal(normal[0],dir);
-	double ps[1] = G3Xprodscal(normal[2],dir);
-	double ps[2] = G3Xprodscal(normal[4],dir);
+	double ps[3] = {0,0,0};
+	ps[0] = G3Xprodscal(normal[0],dir);
+	ps[1] = G3Xprodscal(normal[2],dir);
+	ps[2] = G3Xprodscal(normal[4],dir);
 	
-	int i = 0;
+	int i;
 	for(i = 0; i<3; i++){
-		flag = i*2;
+		int flag = i*2;
 		if(ps[i]>0){
 			flags[flag+1] = 1;
 		}else{
@@ -154,51 +193,44 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
 			switch(i){
 				case 0:
 					k = (0.5-pos[0])/dir[0];
-
-					proj =  {
-						0.5, 
-						k * dir[1] + pos[1], 
-						k * dir[2] + pos[2]};
+					proj[0] = 0.5; 
+					proj[1] = k * dir[1] + pos[1]; 
+					proj[2] = k * dir[2] + pos[2];
 					break;
 				case 1:
 					k = (-0.5-pos[0])/dir[0];
 
-					proj =  {
-						-0.5, 
-						k * dir[1] + pos[1], 
-						k * dir[2] + pos[2]};
+					proj[0] = -0.5; 
+					proj[1] = k * dir[1] + pos[1]; 
+					proj[2] = k * dir[2] + pos[2];
 					break;
 				case 2:
 					k = (0.5-pos[1])/dir[1];
 
-					proj =  {
-						k * dir[0] + pos[0],
-						0.5,  
-						k * dir[2] + pos[2]};
+					proj[0] = k * dir[0] + pos[0];
+					proj[1] = 0.5;  
+					proj[2] = k * dir[2] + pos[2];
 					break;
 				case 3:
 					k = (-0.5-pos[1])/dir[1];
 
-					proj =  {
-						k * dir[0] + pos[0],
-						-0.5,  
-						k * dir[2] + pos[2]};
+					proj[0] = k * dir[0] + pos[0];
+					proj[1] = -0.5; 
+					proj[2] = k * dir[2] + pos[2];
 					break;
 				case 4:
 					k = (0.5-pos[2])/dir[2];
 
-					proj =  {
-						k * dir[0] + pos[0],
-						k * dir[1] + pos[1],
-						0.5};
+					proj[0] = k * dir[0] + pos[0];
+					proj[1] = k * dir[1] + pos[1];
+					proj[2] = 0.5;
 					break;
 				case 5:
 					k = (-0.5-pos[2])/dir[2];
 
-					proj =  {
-						k * dir[0] + pos[0],
-						k * dir[1] + pos[1],
-						-0.5};
+					proj[0] = k * dir[0] + pos[0];
+					proj[1] = k * dir[1] + pos[1];
+					proj[2] = -0.5;
 					break;
 			}
 
@@ -208,7 +240,7 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
 				proj[1] > 0,5 ||
 				proj[2] < -0.5 ||
 				proj[2] > 0,5 ){
-				return 0
+				return 0;
 			}
 		}
 	}
@@ -221,9 +253,11 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
 void doLevel1(int argc,char* argv[]){
 	int i = 0;
 	int j = 0;
+	char * src = getOpt("-i",argc,argv);
+	initObjects(src);
 	for(i = 0; i<32; i++){
 		for(j = 0; j<32; j++){
-
+			
 		}
 	}
 }
