294433d4d76bf07c3c4918d22eb5723a1c28cf82
Steeve Sivanantham <steeve.sivanantham@gmail.com>
2016-04-17 13:53:58 +0200

    Intersection with triangles and rectangles implemented

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <g3x.h>
 
+
+int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint i);
+int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i);
+int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i);
+
 typedef struct{
 	G3Xpoint a;
 	G3Xpoint b;
@@ -9,6 +14,7 @@ typedef struct{
 typedef struct{
 	G3Xpoint centre;
 	double rayon;
+	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 }Sphere;
 
 typedef struct{
@@ -16,12 +22,14 @@ typedef struct{
 	G3Xvector longueur;
 	G3Xvector largeur;
 	G3Xvector hauteur;
+	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 }Rectangle;
 
 typedef struct{
 	G3Xpoint centre;
 	double rayon;
 	G3Xvector hauteur;
+	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 }Cylindre;
 
 typedef struct{
@@ -33,19 +41,20 @@ typedef struct{
 	void *this;
 	G3Xhmat transform;
 	G3Xhmat inverse;
-	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 }Object;
 
 
 Triangle triangle = {	
 	{0,0,0}, 
 	{1,0,0}, 
-	{0,1,0}	
+	{0,1,0},	
+	&(rayInterTriangle)
 };
 
 Sphere sphere = {
 	{0,0,0},
-	1
+	1,
+	&(rayInterCercle)
 };
 
 Rectangle rectangle = {
@@ -53,6 +62,7 @@ Rectangle rectangle = {
 	{1,0,0},
 	{0,1,0},
 	{0,0,1},
+	&(rayInterRectangle)
 };
 
 
@@ -68,7 +78,144 @@ int nbObjects = 0;
 float image[32][32];
 /*****************************************************************************************************/
 
+int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint i){
+	double ps = G3Xprodscal((G3Xvector)pos,dir);
+	if(ps>= 0 ){
+		return 0;
+	}
+	double d2 = G3Xsqrvnorm((G3Xvector)pos) - ps*ps;
+	if(d2>= 1){
+		return 0;
+	}
+	double t = -ps - sqrt(1-d2);
+	/*j'ai mon contact*/
+	i[0] = pos[0] + t * dir[0];
+	i[1] = pos[1] + t * dir[1];
+	i[2] = pos[2] + t * dir[2];
+	return 1;
+}
 
+
+int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
+	G3Xvector normal = {1,0,0};
+	double ps = G3Xprodscal(normal,dir);
+	if(ps>=0){
+		return 0;
+	}
+	double k = (0-pos[0])/dir[0];
+
+	G3Xpoint proj =  {
+		0, 
+		k * dir[1] + pos[1] , 
+		k * dir[2] + pos[2]};
+	if(proj[0] != 0 || 
+		proj[1] < 0 || 
+		proj[2] < 0 || 
+		proj[1] + proj[2] > 1){
+
+		return 0;
+	}
+
+	return 1;
+}
+
+int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint i){
+	G3Xvector normal[6] = {
+		{1,0,0},
+		{-1,0,0},
+		{0,1,0},
+		{0,-1,0},
+		{0,0,1},
+		{0,0,-1}
+	}
+	int flags[6] = {0,0,0,0,0,0};
+	int ps[3] = {0,0,0}
+	double ps[0] = G3Xprodscal(normal[0],dir);
+	double ps[1] = G3Xprodscal(normal[2],dir);
+	double ps[2] = G3Xprodscal(normal[4],dir);
+	
+	int i = 0;
+	for(i = 0; i<3; i++){
+		flag = i*2;
+		if(ps[i]>0){
+			flags[flag+1] = 1;
+		}else{
+			if(ps[i] != 0){
+				flags[flag] = 1;
+			}
+		}
+
+	}
+
+	for(i = 0; i<6; i++){
+		if(flags[i] == 1){
+			double k = 0;
+			G3Xvector proj = {0,0,0};
+			switch(i){
+				case 0:
+					k = (0.5-pos[0])/dir[0];
+
+					proj =  {
+						0.5, 
+						k * dir[1] + pos[1], 
+						k * dir[2] + pos[2]};
+					break;
+				case 1:
+					k = (-0.5-pos[0])/dir[0];
+
+					proj =  {
+						-0.5, 
+						k * dir[1] + pos[1], 
+						k * dir[2] + pos[2]};
+					break;
+				case 2:
+					k = (0.5-pos[1])/dir[1];
+
+					proj =  {
+						k * dir[0] + pos[0],
+						0.5,  
+						k * dir[2] + pos[2]};
+					break;
+				case 3:
+					k = (-0.5-pos[1])/dir[1];
+
+					proj =  {
+						k * dir[0] + pos[0],
+						-0.5,  
+						k * dir[2] + pos[2]};
+					break;
+				case 4:
+					k = (0.5-pos[2])/dir[2];
+
+					proj =  {
+						k * dir[0] + pos[0],
+						k * dir[1] + pos[1],
+						0.5};
+					break;
+				case 5:
+					k = (-0.5-pos[2])/dir[2];
+
+					proj =  {
+						k * dir[0] + pos[0],
+						k * dir[1] + pos[1],
+						-0.5};
+					break;
+			}
+
+			if( proj[0] < -0.5 ||
+				proj[0] > 0,5 ||
+				proj[1] < -0.5 ||
+				proj[1] > 0,5 ||
+				proj[2] < -0.5 ||
+				proj[2] > 0,5 ){
+				return 0
+			}
+		}
+	}
+
+	return 1;
+
+}
 
 /****************************************************************************************************/
 void doLevel1(int argc,char* argv[]){
