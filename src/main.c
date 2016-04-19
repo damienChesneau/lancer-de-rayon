71f6dfef34c6c523a6baf8c842fd16444ff81e08
Steeve Sivanantham <steeve.sivanantham@gmail.com>
2016-04-19 12:12:46 +0200

    Image generated but there is bugs

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <g3x.h>
-
+#include <limits.h>
 
 int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint ri);
 int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri);
@@ -10,6 +10,7 @@ void doLevel1(int argc,char* argv[]);
 void doLevel2(int argc,char* argv[]);
 void doLevel3(int argc,char* argv[]);
 void doLevel4(int argc,char* argv[]);
+void save(char * dst);
 
 typedef struct{
 	G3Xpoint a;
@@ -45,7 +46,8 @@ typedef struct{
 }Rayon;
 
 typedef struct{
-	int this;
+	G3Xcolor color;
+	int (*intersection)(G3Xpoint,G3Xvector,G3Xpoint);
 	G3Xhmat transfo;
 	G3Xhmat inverse;
 }Object;
@@ -79,39 +81,82 @@ Cylindre cylindre = {
 	{0,0,1},
 	NULL
 };
-
-
-
 Object camera;
 Object objects[100];
 int nbObjects = 0;
-float image[32][32];
+G3Xcolor image[32][32];
 /*****************************************************************************************************/
+
+void save(char * dst){
+	FILE* fichier = fopen(dst,"w");
+	fprintf(fichier, "P3\n");
+	fprintf(fichier, "32 32\n");
+	fprintf(fichier, "255\n");	
+	int i = 0;
+	int j = 0;
+	for(i = 0; i<31; i++){
+		for(j = 0; j<31; j++){
+			fprintf(fichier, "%d %d %d\t",(int)image[i][j][0],(int)image[i][j][1],(int)image[i][j][2] );
+		}
+		fprintf(fichier, "%d %d %d\n",(int)image[i][j][0],(int)image[i][j][1],(int)image[i][j][2] );
+	}
+	fprintf(fichier, "%d %d %d",(int)image[i][j][0],(int)image[i][j][1],(int)image[i][j][2] );
+	fclose(fichier);
+}
 
 void initObjects(char* src){
 	FILE* fichier = fopen(src,"r");
 	if (fichier != NULL){
 		int type = 0;
-		while(fscanf(fichier,"%d\t",&type)){
+		while(fscanf(fichier,"%d\t",&type) != EOF){
 			int i = 0;
 			if(type != 4){
-				objects[nbObjects].this = type;
-				for(i = 0; i<16; i++){
+				switch(type){
+					case 0 :
+						objects[nbObjects].intersection = rayInterCercle;
+						objects[nbObjects].color[0] = 255;
+						objects[nbObjects].color[1] = 0;
+						objects[nbObjects].color[2] = 0 ;
+						break;
+					case 1 :
+						objects[nbObjects].intersection = rayInterRectangle;
+						objects[nbObjects].color[0] = 0;
+						objects[nbObjects].color[1] = 255;
+						objects[nbObjects].color[2] = 0;
+						break;
+					case 2 :
+						objects[nbObjects].intersection = rayInterTriangle;
+						objects[nbObjects].color[0] = 0;
+						objects[nbObjects].color[1] = 0;
+						objects[nbObjects].color[2] = 255;
+						break;
+					case 3 :
+						objects[nbObjects].intersection = NULL;
+						objects[nbObjects].color[0] = 255;
+						objects[nbObjects].color[1] = 255;
+						objects[nbObjects].color[2] = 0;
+						break;
+				}
+				for(i = 0; i<15; i++){
 					if(fscanf(fichier,"%lf ",&(objects[nbObjects].transfo[i])));
 				}
-				for(i = 0; i<16; i++){
+
+				if(fscanf(fichier,"%lf  ",&(objects[nbObjects].transfo[i])));
+				for(i = 0; i<15; i++){
 					if(fscanf(fichier,"%lf ",&(objects[nbObjects].inverse[i])));
 				}
+				if(fscanf(fichier,"%lf\n",&(objects[nbObjects].inverse[i])));
 				nbObjects++;
 			}else{
-				for(i = 0; i<16; i++){
+				for(i = 0; i<15; i++){
 					if(fscanf(fichier,"%lf ",&(camera.transfo[i])));
 				}
-				for(i = 0; i<16; i++){
+				if(fscanf(fichier,"%lf  ",&(camera.transfo[i])));
+				for(i = 0; i<15; i++){
 					if(fscanf(fichier,"%lf ",&(camera.inverse[i])));
 				}
+				if(fscanf(fichier,"%lf\n",&(camera.inverse[i])));
 			}
-			fscanf(fichier, "\n");
 		}
 	}
 	fclose(fichier);
@@ -131,6 +176,7 @@ int rayInterCercle(G3Xpoint pos,G3Xvector dir, G3Xpoint ri){
 	ri[0] = pos[0] + t * dir[0];
 	ri[1] = pos[1] + t * dir[1];
 	ri[2] = pos[2] + t * dir[2];
+
 	return 1;
 }
 
@@ -154,7 +200,7 @@ int rayInterTriangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri){
 
 		return 0;
 	}
-
+	ri = proj;
 	return 1;
 }
 
@@ -189,7 +235,7 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri){
 	for(i = 0; i<6; i++){
 		if(flags[i] == 1){
 			double k = 0;
-			G3Xvector proj = {0,0,0};
+			G3Xpoint proj = {0,0,0};
 			switch(i){
 				case 0:
 					k = (0.5-pos[0])/dir[0];
@@ -242,6 +288,7 @@ int rayInterRectangle(G3Xpoint pos, G3Xvector dir, G3Xpoint ri){
 				proj[2] > 0,5 ){
 				return 0;
 			}
+			ri = proj;
 		}
 	}
 
@@ -254,12 +301,50 @@ void doLevel1(int argc,char* argv[]){
 	int i = 0;
 	int j = 0;
 	char * src = getOpt("-i",argc,argv);
+	printf("Option OK\n");
 	initObjects(src);
+	printf("Init Object OK\n");
+	G3Xpoint canFocale = {1,0,0};
 	for(i = 0; i<32; i++){
 		for(j = 0; j<32; j++){
-			
+			G3Xpoint canPixel = {0,i-16,j-16};
+			G3Xpoint pixel;
+			G3Xvector canRay;
+			G3Xvector ray;
+			G3Xsetvct(canRay,canFocale,canPixel);
+			g3x_ProdHMatVector(camera.transfo,canRay,ray);
+			g3x_ProdHMatVector(camera.transfo,canPixel,pixel);
+			int lastInter = INT_MAX;
+			G3Xcolor toSet = {0,0,0};
+			int k = 0;
+
+			for(k = 0; k<nbObjects;k++){
+				G3Xvector inObjectRay;
+				G3Xpoint inObjectPixel;
+				g3x_ProdHMatVector(objects[k].inverse,ray,inObjectRay);
+				g3x_ProdHMatVector(objects[k].inverse,pixel,inObjectPixel);
+				G3Xpoint inObjectRi;
+				G3Xpoint ri;
+				if(objects[k].intersection(inObjectPixel,inObjectRay,inObjectRi) == 1){
+					g3x_ProdHMatVector(objects[k].transfo,inObjectRi, ri);
+					G3Xvector rayInter;
+					G3Xsetvct(pixel,ri,rayInter);
+					if(G3Xsqrvnorm(rayInter)<lastInter){
+						toSet[0] = objects[k].color[0];
+						toSet[1] = objects[k].color[1];
+						toSet[2] = objects[k].color[2];
+
+					}
+				}
+				image[i][j][0] = toSet[0];
+				image[i][j][1] = toSet[1];
+				image[i][j][2] = toSet[2];
+
+			}
+
 		}
 	}
+	save(getOpt("-o",argc,argv));
 }
 
 void doLevel2(int argc,char* argv[]){
